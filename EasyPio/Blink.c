925043c921487cf1f0dc2152395bb71e4bac1284
Carlos Valdez <carlos.valdez.utn@gmail.com>
2019-10-02 20:30:04 -0300

    gpio funcionando, parpadeo todavía no

--- EasyPio/Blink.c
+++ EasyPio/Blink.c
@@ -7,14 +7,18 @@
 void main(void){
 
 	pioInit();		//inicializamos config de puertos
-//	pinMode(21, OUTPUT);	//seleccionamos puertos de salida para los leds
-//	pinMode(20, OUTPUT);
+	pinMode(21, OUTPUT);	//seleccionamos puertos de salida para los leds
+	pinMode(20, OUTPUT);
 	pinMode(16, OUTPUT);
 
 	while(1){
-//		digitalWrite(21, 1);
-//		digitalWrite(20, 1);
+		digitalWrite(21, 1);
+		digitalWrite(20, 1);
 		digitalWrite(16, 1);
+		delayMillis(1000);
+		digitalWrite(21, 0);
+		digitalWrite(20, 0);
+		digitalWrite(16, 0);
 	}
 }
 
