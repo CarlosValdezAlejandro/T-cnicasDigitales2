fbf821a3f7f4743bda25cd6ec7eee2d733d80466
Carlos Valdez <carlos.valdez.utn@gmail.com>
2019-10-08 13:18:22 -0300

    Nuevas carpetas y archivos con ejercicios 2 y 3, sin compilar

---
+++ EasyPio/E2/E2.c
@@ -0,0 +1,32 @@
+//Programa para poner en alto una salida 
+
+#include "EasyPio.h"
+
+
+void main(void){
+
+//inicializo pioInit para configurar registros
+pioInit();
+
+int i=1;
+
+//configuro puertos gpio
+
+pinMode(16, OUTPUT);
+pinMode(21, INPUT);
+pinMode(20, INPUT);
+
+//Bucle para poner en 1 la salida 16, y salir
+//cuando la entrada 21 lea un 1
+while(i){
+
+	digitalWrite(16, digitalRead(21));
+	i = digitalRead(20);
+	
+}
+
+//pone en 0  la salida 16
+digitalWrite(16, 0);
+
+return 0;
+}
