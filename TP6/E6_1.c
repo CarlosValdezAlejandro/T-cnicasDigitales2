f2ab3af498142c6bc536fbbe7d6170fb19c64f91
Carlos Valdez <carlos.valdez.utn@gmail.com>
2019-11-07 14:54:41 -0300

    adaptado raspberry 3B+

--- TP6/E6_1.c
+++ TP6/E6_1.c
@@ -3,33 +3,42 @@
 #include "EasyPio.h"
 
 #define NUM_OUTS 8
-#define NUM_IN   2
+//#define NUM_IN   1
 
 int main(void){
 
-int outs[NUM_OUTS] = {x, x, x, x, x, x, x, x};
-int ins[NUM_IN] = {x, x};
+int  i, j;
 
-vector [NUM_OUTS][NUM_OUTS] = { {1,0,0,0,0,0,0,1},
-			        {0,1,0,0,0,0,1,0},
-		      	        {0,0,1,0,0,1,0,0},
-			        {0,0,0,1,1,0,0,0},
-			        {0,0,0,1,1,0,0,0},
-			        {0,0,1,0,0,1,0,0},
-			        {0,1,0,0,0,0,1,0},
-			        {1,0,0,0,0,0,0,1} };
+int outs[NUM_OUTS] = {23, 24, 25, 12, 16, 20, 21, 26};
+//int ins[NUM_IN] = {17};
+
+int vector [8][8] = { {1,0,0,0,0,0,0,0},
+		      {0,1,0,0,0,0,0,0},
+		      {0,0,1,0,0,0,0,0},
+		      {0,0,0,1,0,0,0,0},
+		      {0,1,0,0,1,0,0,0},
+		      {0,0,0,1,0,1,0,0},
+		      {0,0,0,0,0,1,1,0},
+		      {0,0,0,0,0,0,0,1} };
 pioInit();
 
-pinMode_out(outs[], NUM_OUTS);
-pinMode_in(ins[], NUM_INS);
+pinMode_out(outs, NUM_OUTS);
+//pinMode_in(ins, NUM_INS);
 
-while(1){
 
-	for(int i=0; i<NUM_OUTS; i++){
-		for(int j; j<NUM_OUTS; j++){
+
+	for(i=0; i<NUM_OUTS; i++){
+		for(j=0; NUM_OUTS > j; j++){
+		//	digitalWrite(outs[0], 1);
 			digitalWrite(outs[j], vector[i][j]);
-	}}
-}
+		//	k=digitalRead(17);
+			
+		}
+			delayMillis(500);
+	}
+
+
+//digitalWrite_s(outs, NUM_OUTS, 0);
 
 return 0;
 }
